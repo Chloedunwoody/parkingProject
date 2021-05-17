9346e73b4772a08ced418bf5f46600e1c6df5a8e
Chloedunwoody <54686732+Chloedunwoody@users.noreply.github.com>
2021-05-17 17:16:10 -0400

    table collumns
    reintroduced table

--- public/scripts/login.php
+++ public/scripts/login.php
@@ -18,14 +18,14 @@ session_start();
         $_SESSION['username'] = $user;
         header("location: entry.php");
         ?>
-        <!--        <div style="border:1px solid #333; background-color:#fbd2d7; border-radius:5px; padding:16px;" align="center">-->
-        <!--            <h4>--><?php //echo $row["id"]; ?><!--</h4>-->
-        <!--            <h4>--><?php //echo $row["firstName"]; ?><!--</h4>-->
-        <!--            <h4>--><?php //echo $row["lastName"]; ?><!--</h4>-->
-        <!--            <h4> --><?php //echo $row["userName"]; ?><!--</h4>-->
-        <!--            <h4> --><?php //echo $row["password"]; ?><!--</h4>-->
-        <!--            <h4>--><?php //echo $row["email"]; ?><!--</h4>-->
-        <!--        </div>-->
+                <div style="border:1px solid #333; background-color:#fbd2d7; border-radius:5px; padding:16px;" align="center">
+                    <h4><?php echo $row["id"]; ?></h4>
+                    <h4><?php echo $row["firstName"]; ?></h4>
+                    <h4><?php echo $row["lastName"]; ?></h4>
+                    <h4> <?php echo $row["userName"]; ?></h4>
+                    <h4> <?php echo $row["password"]; ?></h4>
+                    <h4><?php echo $row["email"]; ?></h4>
+                </div>
         <?php
     }
     else {
